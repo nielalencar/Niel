5eda5233d30e78ec48278301f000b6eeee6d5552
NIEL <129704411+nielalencar@users.noreply.github.com>
2023-11-30 00:10:08 -0400

    Installed react-router-dom, created the routes folder to define the route components and implemented it

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const options = [
   {
     name: "Home",
@@ -23,7 +25,7 @@ export function NavgationBar() {
       <ul className="flex justify-center gap-2">
         {options.map((option) => (
           <li key={`${option.name}-${option.href}`}>
-            <a href={option.href} className="text-white text-xs lg:text-base font-roboto font-normal bg-neutral-700 focus:bg-neutral-800 lg:hover:bg-neutral-800 px-3 lg:px-4 py-1 rounded">{option.name}</a>
+            <Link to={option.href} className="text-white text-xs lg:text-base font-roboto font-normal bg-neutral-700 focus:bg-neutral-800 lg:hover:bg-neutral-800 px-3 lg:px-4 py-1 rounded">{option.name}</Link>
           </li>
         ))}
       </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { Welcome } from './routes/Welcome.jsx'
+import { Projects } from './routes/Projects.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Welcome />
+      },
+      {
+        path: "/projetos",
+        element: <Projects />
+      },
+    ]
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
