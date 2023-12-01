43975e3eeaeb0e573bea42642c0852d973a00b56
NIEL <129704411+nielalencar@users.noreply.github.com>
2023-12-01 16:39:22 -0400

    Change in header navigation bar flex

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -3,7 +3,7 @@ import { NavgationBar } from "../../components/Menu";
 
 export function Header() {
   return (
-    <header className="pt-[62px] md:pt-[92px] flex flex-col gap-5 mx-8 md:mx-14 lg:mx-40 xl:mx-48 2xl:mx-96 pb-5 md:pb-10 border-b border-neutral-700 lg:flex-row lg:items-end lg:justify-center">
+    <header className="pt-[62px] md:pt-[92px] flex flex-col gap-5 lg:gap-6 mx-8 md:mx-14 lg:mx-40 xl:mx-48 2xl:mx-96 pb-5 md:pb-10 border-b border-neutral-700">
       <Profile />
       <NavgationBar />
     </header>
